30d7494064dddbef4d23fc31ed83cb6b5fd1f6b4
Abhay Sahani <abhay.cs.777@gmail.com>
2024-02-26 22:07:17 +0530

    same tree

--- LeetCode Daily/Same-tree.cpp
+++ LeetCode Daily/Same-tree.cpp
@@ -13,3 +13,12 @@ class Solution {
            isSameTree(p->right, q->right);
   }
 };
+
+// Now let's test the program
+int main() {
+  Solution* obj = new Solution();
+  TreeNode* p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+  TreeNode* q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+  cout << obj->isSameTree(p, q) << endl;
+  return 0;
+}
