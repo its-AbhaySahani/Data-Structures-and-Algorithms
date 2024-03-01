2a71d55d0271f4357db3172e51945a7706fe30b7
Abhay Sahani <abhay.cs.777@gmail.com>
2024-03-01 21:06:35 +0530

    1st march

---
+++ LeetCode Daily/Max-odd-binary-number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+class Solution {
+ public:
+  string maximumOddBinaryNumber(string s) {
+    const int zeros = ranges::count(s, '0');
+    const int ones = s.length() - zeros;
+    return string(ones - 1, '1') + string(zeros, '0') + '1';
+  }
+};
