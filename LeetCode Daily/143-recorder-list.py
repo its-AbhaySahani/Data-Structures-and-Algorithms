4baa21d14553b2134efcd4d589f05bf3157903da
Abhay Sahani <abhay.cs.777@gmail.com>
2024-03-23 22:40:23 +0530

    23 march

---
+++ LeetCode Daily/143-recorder-list.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+import collections
+from typing import List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+  def reorderList(self, head: ListNode) -> None:
+    def findMid(head: ListNode):
+      prev = None
+      slow = head
+      fast = head
+
+      while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+      prev.next = None
+
+      return slow
+
+    def reverse(head: ListNode) -> ListNode:
+      prev = None
+      curr = head
+
+      while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+
+      return prev
+
+    def merge(l1: ListNode, l2: ListNode) -> None:
+      while l2:
+        next = l1.next
+        l1.next = l2
+        l1 = l2
+        l2 = next
+
+    if not head or not head.next:
+      return
+
+    mid = findMid(head)
+    reversed = reverse(mid)
+    merge(head, reversed)
