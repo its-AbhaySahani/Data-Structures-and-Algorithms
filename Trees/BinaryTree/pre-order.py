57a3755c3e93285b84989fbb97cfbf7159c76d8a
Abhay Sahani <abhay.cs.777@gmail.com>
2024-03-26 16:00:27 +0530

    pre-order tetsing

--- Trees/BinaryTree/pre-order.py
+++ Trees/BinaryTree/pre-order.py
@@ -12,3 +12,13 @@ def printPreorder(root):
         print(root.val),
         printPreorder(root.left)
         printPreorder(root.right)
+
+# Test the function
+root = node(1)
+root.left = node(2)
+root.right = node(3)
+root.left.left = node(4)
+root.left.right = node(5)
+
+print("Preorder traversal of binary tree is")
+printPreorder(root)
