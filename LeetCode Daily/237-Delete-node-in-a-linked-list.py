85147832bd7878cf60ebec5717f984dc87f204da
Abhay Sahani <abhay.cs.777@gmail.com>
2024-05-05 21:39:33 +0530

    237

--- LeetCode Daily/237-Delete-node-in-a-linked-list.py
+++ LeetCode Daily/237-Delete-node-in-a-linked-list.py
@@ -9,3 +9,9 @@ class Solution:
     def deleteNode(self, node):
         node.val = node.next.val
         node.next = node.next.next
+
+# lets test the solution
+node = linkedListNode(4)
+node.next = linkedListNode(5)
+node.next.next = linkedListNode(1)
+node.next.next.next = linkedListNode(9)
