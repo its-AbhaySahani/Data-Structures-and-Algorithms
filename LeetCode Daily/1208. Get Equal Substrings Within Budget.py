e841d6c6146fe053875015b3164025aa2deb83de
Abhay Sahani <abhay.cs.777@gmail.com>
2024-05-29 00:16:43 +0530

    1208

---
+++ LeetCode Daily/1208. Get Equal Substrings Within Budget.py
@@ -0,0 +1,12 @@
+import math
+
+class Solution:
+  def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+    j = 0
+    for i in range(len(s)):
+      maxCost -= abs(ord(s[i]) - ord(t[i]))
+      if maxCost < 0:
+        maxCost += abs(ord(s[j]) - ord(t[j]))
+        j += 1
+
+    return len(s) - j
