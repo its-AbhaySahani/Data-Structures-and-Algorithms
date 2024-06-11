d445acb4ef27fdaf62204df5fc11c66a107c80e2
Abhay Sahani <abhay.cs.777@gmail.com>
2024-06-12 02:08:24 +0530

    1122

--- LeetCode Daily/1122. Relative Sort Array.py
+++ LeetCode Daily/1122. Relative Sort Array.py
@@ -18,3 +18,8 @@ class Solution:
         ans.append(num)
 
     return ans
+  
+# lets test the function
+arr1 = [2,3,1,3,2,4,6,7,9,2,19]
+arr2 = [2,1,4,3,9,6]
+print(Solution().relativeSortArray(arr1, arr2))
