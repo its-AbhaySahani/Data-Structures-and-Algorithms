c36dbfbee47c455256fb9f6048ebc88a05c743d8
Abhay Sahani <abhay.cs.777@gmail.com>
2024-06-14 00:52:49 +0530

    2037

---
+++ LeetCode Daily/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <ranges>
+
+class Solution {
+ public:
+  int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+    int ans = 0;
+
+    ranges::sort(seats);
+    ranges::sort(students);
+
+    for (int i = 0; i < seats.size(); ++i)
+      ans += abs(seats[i] - students[i]);
+
+    return ans;
+  }
+};
