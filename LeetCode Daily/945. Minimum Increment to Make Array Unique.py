f1da4955e74669224420f9cd999aa2b1dc0d6465
Abhay Sahani <abhay.cs.777@gmail.com>
2024-06-14 13:59:46 +0530

    945

---
+++ LeetCode Daily/945. Minimum Increment to Make Array Unique.py
@@ -0,0 +1,14 @@
+import collections
+from typing import List
+class Solution:
+  def minIncrementForUnique(self, nums: List[int]) -> int:
+    ans = 0
+    minAvailable = 0
+
+    for num in sorted(nums):
+      ans += max(minAvailable - num, 0)
+      minAvailable = max(minAvailable, num) + 1
+
+    return ans
+  
+  
