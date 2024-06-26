105ed61587cdf70a2dbc8d0bc14366afcee3fdd2
Abhay Sahani <abhay.cs.777@gmail.com>
2024-06-26 23:55:33 +0530

    22 leetcode

---
+++ Important Questions/generate-parenthesis.py
@@ -0,0 +1,17 @@
+# generate parenthesis 
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+import sys
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(s = '', left = 0, right = 0):
+            if len(s) == 2 * n:
+                ans.append(s)
+                return
+            if left < n:
+                backtrack(s+'(', left+1, right)
+            if right < left:
+                backtrack(s+')', left, right+1)
+        ans = []
+        backtrack()
+        return ans
