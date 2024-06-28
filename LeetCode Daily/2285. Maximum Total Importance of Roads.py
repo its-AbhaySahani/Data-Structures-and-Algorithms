e2b2b449acfd922a06fb3129b79fcd2b48bf6d8b
Abhay Sahani <abhay.cs.777@gmail.com>
2024-06-29 01:18:44 +0530

    2285

--- LeetCode Daily/2285. Maximum Total Importance of Roads.py
+++ LeetCode Daily/2285. Maximum Total Importance of Roads.py
@@ -10,3 +10,8 @@ class Solution:
 
     count.sort()
     return sum((i + 1) * c for i, c in enumerate(count))
+
+# Let's test the solution
+n = 4
+roads = [[1, 0], [2, 0], [3, 0]]
+print(Solution().maximumImportance(n, roads)) 
