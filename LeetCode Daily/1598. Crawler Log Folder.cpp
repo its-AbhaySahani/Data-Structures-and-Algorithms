76c7c2d506f4c17d1c7be660eca59435d762d22a
Abhay Sahani <abhay.cs.777@gmail.com>
2024-07-10 20:02:04 +0530

    1598

---
+++ LeetCode Daily/1598. Crawler Log Folder.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<vector>
+#include<string>
+class Solution {
+ public:
+  int minOperations(vector<string>& logs) {
+    int ans = 0;
+
+    for (const string& log : logs) {
+      if (log == "./")
+        continue;
+      if (log == "../")
+        ans = max(0, ans - 1);
+      else
+        ++ans;
+    }
+
+    return ans;
+  }
+};
