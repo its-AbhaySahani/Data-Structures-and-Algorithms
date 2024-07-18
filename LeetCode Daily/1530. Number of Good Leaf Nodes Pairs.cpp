e1e9345cc7ec863f3941728de2cc53090ce17f75
Abhay Sahani <abhay.cs.777@gmail.com>
2024-07-19 00:03:39 +0530

    1530

---
+++ LeetCode Daily/1530. Number of Good Leaf Nodes Pairs.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<vector>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int countPairs(TreeNode* root, int distance) {
+    int ans = 0;
+
+    dfs(root, distance, ans);
+
+    return ans;
+  }
+
+ private:
+  vector<int> dfs(TreeNode* root, int distance, int& ans) {
+    vector<int> d(distance + 1);  // {distance: the number of leaf nodes}
+    if (root == nullptr)
+      return d;
+    if (root->left == nullptr && root->right == nullptr) {
+      d[0] = 1;
+      return d;
+    }
+
+    const vector<int> dl = dfs(root->left, distance, ans);
+    const vector<int> dr = dfs(root->right, distance, ans);
+
+    for (int i = 0; i < distance; ++i)
+      for (int j = 0; j < distance; ++j)
+        if (i + j + 2 <= distance)
+          ans += dl[i] * dr[j];
+
+    for (int i = 0; i < distance; ++i)
+      d[i + 1] = dl[i] + dr[i];
+
+    return d;
+  }
+};
